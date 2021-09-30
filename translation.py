ca566f96321e77f80e6847e9551f37eccf0d7f0c
AnimeSigma <91663967+AnimeSigma@users.noreply.github.com>
2021-09-30 20:06:06 +0530

    Sigma

--- translation.py
+++ translation.py
@@ -1,23 +1,23 @@
 class Translation(object):
-    START_TEXT = """ <b>OwO Hey There {}, \nI'm Ai Hayasaka VN's Waifu & A Bot, I can Rename+Convert Multiple Files At Once With Permanent Thumbnail And Custom Caption support Yay!😁</b> \n
+    START_TEXT = """ <b>Sup? {}, \nI'm Kurisu Makise Waifu Of Many Oof, Well I'm Also A Bot...I can Rename+Convert Multiple Files At Once With Permanent Thumbnail And Custom Caption support Yay!😁</b> \n
 
-<b>*I am Well Maintained By*: @Vedant_vn 👀✌ </b> \n 
+<b>*Currently I am Maintained By*: @Vedant_vn✨ </b> \n 
 """
 
     BANNED_USER_TEXT = "Lol My Owner Has Banned You From Using Me. That Means You Can't Run Me Now! \n \n Contact : @NarutoRobot_Support To Know More..." 
-    DOWNLOAD_START = "<b>Oh ya Baby OvO, Downloading Your File To My server! Just Wait a Bit</b>"
-    UPLOAD_START = "<b>Downloading Is Now Completed,Okay I'm Uploading Your File into Our PM 😉</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Heya Thank you for Using Me Support Our Channel @AnimeSigma❤️</b>"
+    DOWNLOAD_START = "<b>Hmm Wait,I'm Downloading Your File To My server!</b>"
+    UPLOAD_START = "<b>Downloading Is Now Completed,Okay I'm Uploading Your File into your PM 👀</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Ok Thank you for Using Me Now Support Our Channel @AnimeSigma❤️</b>"
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Ok Reeee...thumbnail cleared succesfully🤦 Khush Abb?"
-    SAVED_RECVD_DOC_FILE = "<b>Yay File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Oi Noob Baka Reply with /rename file name & extension To rename a file</b>"
-    REPLY_TO_FILE_FOR_CONVERT = "<b>Hmm Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Ok Man...Your thumbnail cleared succesfully🤦 Don't Do it again"
+    SAVED_RECVD_DOC_FILE = "<b> Yeah File Downloaded Successfully🌟</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Bruh Reply with /rename file name & extension To rename a file</b>"
+    REPLY_TO_FILE_FOR_CONVERT = "<b>Arey Reply To An File With /c2v To Convert It Into Streamable video File</b>"
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_THUMB_FOUND = "Aur Bhai Thumbnail Kab Daaloge?"
-    IFLONG_FILE_NAME = """Oi Pagal...Decrease The Number Of Letters,Chutiya Salla 🙂"""
-    ABOUT_ME = """<b>UwU \n Hey I'm Ai Hayasaka 🙈 Just Another Telegram File Renamer Bot Which Can Rename Telegram Files. \n Well,I Support Permanent Thumbnail Feature.\n \n i can also convert files into videos \n Still Got Any Issues With Me HUH?🤨 Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers 🙂 </b>"""
-    HELP_USER = """Checkout The Available Commands Here \n\nDo Join @AnimeSigma If You Found Me Useful Darling❤️"""
+    NO_THUMB_FOUND = "Thumbnail Bhi Mai Daalu Abb?🙂 "
+    IFLONG_FILE_NAME = """Yo Kiddo...Decrease The Number Of Letters"""
+    ABOUT_ME = """<b>Sup? \n I'm Makise Kurisu 😎 Just Another Telegram File Renamer Bot Which Can Rename Telegram Files. \n Well,I Support Permanent Thumbnail Feature.\n \n i can also convert files into videos \n Still Got Any Issues With Me IDC, You Can Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers 🙂 </b>"""
+    HELP_USER = """Checkout The Available Commands Here \n\nDo Join @AnimeSigma If You Found Me Useful 😇"""
     RENAME_HELP = """Here are The Available Commands In Rename \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
     THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
